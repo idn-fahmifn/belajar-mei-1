dd3cf465acc2fd394404696bf22e188ad87212aa
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-05-05 14:57:40 +0700

    1.6

---
+++ resources/views/umur/form.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form Umur</title>
+</head>
+<body>
+    <h1>Form Cek Umur</h1>
+    <p>Masukan umur kamu dibawah ini : </p>
+
+    <form action="">
+        <div class="">
+            <label for="">Nama Lengkap</label>
+            <input type="text" name="nama">
+        </div>
+
+        <div class="">
+            <label for="">Umur</label>
+            <input type="number" name="umur">
+        </div>
+
+        <div class="">
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+
+</body>
+</html>
